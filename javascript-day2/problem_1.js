143c96d23c086f6810fd8428f444522a2832d5e2
Sommin <bliss220@yonsei.ac.kr>
2026-06-09 13:39:33 +0900

    DAY2-problem1

---
+++ javascript-day2/problem_1.js
@@ -0,0 +1,10 @@
+function countOddNumbers(limitValue) {
+  let value = Number(limitValue) || 0;
+  value = value < 1 ? 0 : value;
+  value = value % 1 === 0 ? value : 0;
+
+  if (value) {
+    return Math.floor((value + 1) / 2);
+  } else return value;
+}
+console.log(countOddNumbers("-10"));
